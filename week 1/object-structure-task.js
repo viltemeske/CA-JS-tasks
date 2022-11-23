b7a604f1525a439c0e93ea788f5b3421e518f82b
viltemeske <meshkenai@gmail.com>
2022-11-23 16:10:31 +0200

    week 1: obj(1)

---
+++ week 1/object-structure-task.js
@@ -0,0 +1,69 @@
+const obj = {
+    y: ' ',
+    a: {
+      g: 'proga',
+      p: 'Lietuva',
+      l: ['Kėglis']
+    },
+    b: 'Labas',
+    c: [
+      'Ponas',
+      'mėgstamiausia',
+      {
+        k: '.',
+        t: '!'
+      }
+    ],
+    e: {
+      d: 'žalia',
+      l: 'šalis',
+      a: [
+        {
+          a: 'geras',
+          v: 'galbūt',
+          p: ['susitikti']
+        }
+      ]
+    },
+    f: [
+      { a: ['šiandien', 'gera', 'mano', 'visai'] },
+      {
+        l: 'spalva',
+        a: {
+          s: 'kurioje',
+          u: 'oras',
+          o: ['?']
+        },
+        s: 'gyvenu'
+      }
+    ],
+    l: 'yra'
+  }
+  
+  console.group('Lietuva yra šalis kurioje gyvenu.');
+  {
+    const result = obj.a.p + obj.y + obj.l + obj.y + obj.e.l + obj.y + obj.f[1].a.s + obj.y + obj.f[1].s;
+    console.log(result);
+  }
+  console.groupEnd();
+  
+  console.group('mano mėgstamiausia spalva yra žalia.');
+  {
+    const result = '';
+    console.log(result);
+  }
+  console.groupEnd();
+  
+  console.group('šiandien visai geras oras!');
+  {
+    const result = '';
+    console.log(result);
+  }
+  console.groupEnd();
+  
+  console.group('galbūt gera proga susitikti?');
+  {
+    const result = '';
+    console.log(result);
+  }
+  console.groupEnd();

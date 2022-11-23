21cb8203e694a6cf9dec6dcda1ff00bcf11efd83
viltemeske <meshkenai@gmail.com>
2022-11-23 14:28:50 +0200

    correction(2)

--- week 1/arrays.js
+++ week 1/arrays.js
@@ -35,12 +35,19 @@ let sentence1 = helpers[3] + helpers[7] + nouns[1] + helpers[7] + helpers[4] + h
 console.log(sentence1);
 // 2. hammer is on the table
 let sentence2 = nouns[2] + helpers[7] + helpers[4] + helpers[7] + helpers[2] + helpers[7] + helpers[6] + helpers[7] + nouns[4];
+console.log(sentence2);
 // 3. sun is in the sky
 let sentence3 = nouns[3] + helpers[7] + helpers[4] + helpers[7] + helpers[0] + helpers[7] + helpers[6] + helpers[7] + nouns[5];
+console.log(sentence3);
 7// 4. apple is with my sister
 let sentence4 = nouns[0] + helpers[7] + helpers[4] + helpers[7] + helpers[1] + helpers[7] + helpers[3] + helpers[7] + nouns[1];
+console.log(sentence4);
 // 5. Papildyti masyvą žodžiais, ir sukurti dar 3 +-logiškus sakinius.
 
 let sentence5 = helpers[3] + helpers[7] + nouns[8] + helpers[7] + helpers[4] + helpers[7] + helpers[2] + helpers[7] + helpers[6] + helpers[7] + nouns[4];
+console.log(sentence5);
 let sentence6 = helpers[3] + helpers[7] + nouns[7] + helpers[7] + helpers[4] + helpers[7] + helpers[0] + helpers[7] + helpers[6] + helpers[7] + nouns[6];
+console.log(sentence6);
 let sentence7 = helpers[3] + helpers[7] + adjectives [0] + nouns[9] + helpers[4] + helpers[7] + nouns[0];
+console.log(sentence7);
+

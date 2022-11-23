e80d5f853a791353823af3483d8619b1778a31b3
viltemeske <meshkenai@gmail.com>
2022-11-23 16:30:01 +0200

    week 1: obj(3)

--- week 1/object-structure-task.js
+++ week 1/object-structure-task.js
@@ -56,7 +56,7 @@ const obj = {
   
   console.group('šiandien visai geras oras!');
   {
-    const result = '';
+    const result = obj.f[0].a[0] + obj.y + obj.f[0].a[3] + obj.y + obj.e.a[0].a + obj.y + obj.f[1].a.u + obj.c[2].t;
     console.log(result);
   }
   console.groupEnd();

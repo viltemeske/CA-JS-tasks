ba3d7a5424fb8a3a864f10c3e79f650ae3830226
viltemeske <meshkenai@gmail.com>
2022-11-23 14:21:57 +0200

    5/5

--- week 1/arrays.js
+++ week 1/arrays.js
@@ -2,9 +2,10 @@
 // Masyvo elementai yra numeruojami nuo 0, pvz.:
 let numbers = [46, 1, 4, 78, 9, 5, 1, 4, 5, 5];
 // indeksai:    0  1  2   3  4  5  6  7  8  9
-let nouns = ['apple', 'sister', 'hammer', 'sun', 'table', 'sky', 'lake'];
-// indeksai:    0       1         2        3       4        5       6    
+let nouns = ['apple', 'sister', 'hammer', 'sun', 'table', 'sky', 'lake', 'dog', 'cat', 'food'];
+// indeksai:    0       1         2        3       4        5       6      7       8      9
 let helpers = ['in', 'with', 'on', 'my', 'is', 'a', 'the', ' '];
+let adjectives = ['favourite'];
 // indeksai:    0       1     2      3     4    5     6     7  
 // ---- ---- ---- MASYVO REIKŠMIŲ NAUDOJIMAS
 // Masyvo reikšmės yra pasiekiamas naudojant atitinkamą indeksą:
@@ -39,3 +40,7 @@ let sentence3 = nouns[3] + helpers[7] + helpers[4] + helpers[7] + helpers[0] + h
 7// 4. apple is with my sister
 let sentence4 = nouns[0] + helpers[7] + helpers[4] + helpers[7] + helpers[1] + helpers[7] + helpers[3] + helpers[7] + nouns[1];
 // 5. Papildyti masyvą žodžiais, ir sukurti dar 3 +-logiškus sakinius.
+
+let sentence5 = helpers[3] + helpers[7] + nouns[8] + helpers[7] + helpers[4] + helpers[7] + helpers[2] + helpers[7] + helpers[6] + helpers[7] + nouns[4];
+let sentence6 = helpers[3] + helpers[7] + nouns[7] + helpers[7] + helpers[4] + helpers[7] + helpers[0] + helpers[7] + helpers[6] + helpers[7] + nouns[6];
+let sentence7 = helpers[3] + helpers[7] + adjectives [0] + nouns[9] + helpers[4] + helpers[7] + nouns[0];

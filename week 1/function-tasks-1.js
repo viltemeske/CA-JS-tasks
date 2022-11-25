a995124d7ad2b36cf271ea5c1b7eb95ceb7c4d4a
viltemeske <meshkenai@gmail.com>
2022-11-25 15:49:05 +0200

    week 1: function (4)

--- week 1/function-tasks-1.js
+++ week 1/function-tasks-1.js
@@ -2,75 +2,90 @@ console.groupCollapsed('1. Parašykite funkciją, kuri grąžina pirmą masyvo e
 {
     function returnFirstArrayElement(array) {
         return array[0];
-        }
+    }
 
-        const res1 = returnFirstArrayElement(['pirmas', 'antras', 'trečias']);
-        const res2 =  returnFirstArrayElement([1, 2, 3]);
+    const res1 = returnFirstArrayElement(['pirmas', 'antras', 'trečias']);
+    const res2 = returnFirstArrayElement([1, 2, 3]);
 
-        console.log({res1, res2});
+    console.log({ res1, res2 });
 }
 console.groupEnd();
 
 console.groupCollapsed('2. Parašykite funkciją, kuri pašalina pirmą masyvo elementą ir jį grąžina.');
 {
-  function deleteFirstArrayElement(array) {
-    const firstElement = array[0];
-    delete array[0];
-    for (let i = 1; i < array.length; i++) {
-        array[i - 1] - array[i];
+    function deleteFirstArrayElement(array) {
+        const firstElement = array[0];
+        delete array[0];
+        for (let i = 1; i < array.length; i++) {
+            array[i - 1] - array[i];
+        }
+        array.length - array.length - 1;
+
+        return firstElement;
     }
-    array.length - array.length - 1;
 
-    return firstElement;
-  }
-
-  const numbers = [1, 2, 3, 4, 5];
-  console.log('numbers before remuval', numbers);
-  const firstElement = deleteFirstArrayElement(numbers);
-  console.log('numbers after removal', numbers);
-  console.log('removed element', firstElement)
+    const numbers = [1, 2, 3, 4, 5];
+    console.log('numbers before remuval', numbers);
+    const firstElement = deleteFirstArrayElement(numbers);
+    console.log('numbers after removal', numbers);
+    console.log('removed element', firstElement)
 }
 console.groupEnd();
 
 console.groupCollapsed('3. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.');
 {
     function returnLastArrayElement(array) {
-        return array[array.length-1];
+        return array[array.length - 1];
     }
     const res1 = returnLastArrayElement(['pirmas', 'antras', 'trečias']);
-    const res2 =  returnLastArrayElement([1, 2, 3]);
+    const res2 = returnLastArrayElement([1, 2, 3]);
 
-    console.log({res1, res2});
+    console.log({ res1, res2 });
 }
 console.groupEnd();
 
 console.groupCollapsed('4. Parašykite funkciją, kuri pašalina paskutinį masyvo elementą ir jį grąžina.');
 {
-  // ...sprendimas ir spausdinimas
+    function deleteLastArrayElement(array) {
+        const lastElement = array[array.length - 1];
+        delete array[array.length - 1];
+        for (let i = array.length; i >= 0; i--) {
+            array[i - 1] - array[i];
+        }
+        array.length - array.length - 1;
+
+        return lastElement;
+    }
+
+    const numbers = [1, 2, 3, 4, 5];
+    console.log('numbers before remuval', numbers);
+    const lastElement = deleteLastArrayElement(numbers);
+    console.log('numbers after removal', numbers);
+    console.log('removed element', lastElement)
 }
 console.groupEnd();
 
 console.groupCollapsed('5. Parašykite funkciją, kuri grąžina elementų kiekį masyve');
 {
-  // ...sprendimas ir spausdinimas
+    // ...sprendimas ir spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('6. Parašykite funkciją, kuri atspausdina paskutinio masyvo elemento indeksą');
 {
-  // ...sprendimas ir spausdinimas
+    // ...sprendimas ir spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('7. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus eilutėmis');
 {
-  // ...sprendimas ir spausdinimas
+    // ...sprendimas ir spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('8. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes eilutėmis');
 {
-  // ...sprendimas ir spausdinimas
+    // ...sprendimas ir spausdinimas
 }
 console.groupEnd();
 
@@ -79,31 +94,31 @@ console.groupCollapsed('9. Parašykite funkciją, kuri atspausdina kiekvieno mas
 // [1] => reikšmė
 // [2] => reikšmė
 {
-  // ... sprendimas ir spausdinimas
+    // ... sprendimas ir spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('10. Parašykite funkciją, kuri atspausdina masyvo elementus atbuline tvarka eilutėmis, iš galo.');
 {
-  // ...sprendimas ir spausdinimas
+    // ...sprendimas ir spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('11. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus vienoje eilutėje: 0 1 2 3 ...');
 {
-  // ...sprendimas ir spausdinimas
+    // ...sprendimas ir spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('12. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes vienoje eilutėje: -111 2 -9 48');
 {
-  // ...sprendimas ir spausdinimas
+    // ...sprendimas ir spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('13. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras vienoje eilutėje, tokiu formatu:');
 //  [0]=>17 [1]=>8 [2]=>88 ..
 {
-  // ...sprendimas ir spausdinimas
+    // ...sprendimas ir spausdinimas
 }
 console.groupEnd();

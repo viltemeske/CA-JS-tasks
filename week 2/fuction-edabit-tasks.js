af9b8f4e2734c6a59edfa67b1b563d19cfeda5bb
viltemeske <meshkenai@gmail.com>
2022-11-28 15:36:39 +0200

    week 2: edabit (1)

---
+++ week 2/fuction-edabit-tasks.js
@@ -0,0 +1,219 @@
+console.groupCollapsed('https://edabit.com/challenge/ARr5tA458o2tC9FTN')
+{
+    function hello() {
+        return "hello edabit.com"
+    }
+
+const answer = hello();
+console.log(answer);
+
+}
+console.groupEnd()
+
+console.groupCollapsed('https://edabit.com/challenge/QaApgtePE6QrCZ64o')
+{
+    function getFirstValue(arr) {
+        return arr[0];
+    }
+
+    arr1 = [1, 2, 3];
+    arr2 = [80, 5, 100];
+    arr3 = [-500, 0, 50];
+    console.log(getFirstValue(arr1));
+    console.log(getFirstValue(arr2));
+    console.log(getFirstValue(arr3));
+
+}
+console.groupEnd()
+
+console.groupCollapsed('https://edabit.com/challenge/8q54MKnRrm89pSLmW')
+{
+    function convert(minutes) {
+        return minutes * 60;
+    }
+
+    console.log(convert(5));
+    console.log(convert(3));
+    console.log(convert(2));
+
+}
+console.groupEnd()
+
+console.groupCollapsed('https://edabit.com/challenge/nhXofMMyrowMyr9Nv')
+{
+    function nextEdge(side1, side2) {
+        return (side1 + side2) - 1;
+    }
+
+console.log(nextEdge(8, 10));
+console.log(nextEdge(5, 7));
+console.log(nextEdge(9, 2));
+
+}
+console.groupEnd()
+
+console.groupCollapsed('https://edabit.com/challenge/6AnQqiEjkJdZrWhPS')
+{
+    function howManySeconds(hours) {
+        return hours*3600;
+    }
+    console.log(howManySeconds(2));
+    console.log(howManySeconds(10));
+    console.log(howManySeconds(24));
+
+}
+console.groupEnd()
+
+console.groupCollapsed('https://edabit.com/challenge/Q2j5FTFtsk7PdzrQk')
+{
+    function remainder(x, y) {
+        return x % y; 
+    }
+    console.log(remainder(1, 3));
+    console.log(remainder(3, 4));
+    console.log(remainder(-9, 45));
+    console.log(remainder(5, 5));
+
+}
+console.groupEnd()
+
+console.groupCollapsed('https://edabit.com/challenge/ks3vMrqdnW3CQ3F4i')
+{
+    function squared(b) {
+        return b * b
+    }
+    console.log(squared(5));
+    console.log(squared(9));
+    console.log(squared(100));
+
+}
+console.groupEnd()
+
+console.groupCollapsed('https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw')
+{
+    function calcAge(age) {
+        return age * 365;
+    }
+    console.log(calcAge(65));
+    console.log(calcAge(0));
+    console.log(calcAge(20));
+}
+console.groupEnd()
+
+console.groupCollapsed('https://edabit.com/challenge/wAdE9te55cowBLcPs')
+{
+    function circuitPower(voltage, current) {
+        return voltage * current;
+    }
+    console.log(circuitPower(230, 10));
+    console.log(circuitPower(110, 3));
+    console.log(circuitPower(480, 20));
+
+}
+console.groupEnd()
+
+console.groupCollapsed('https://edabit.com/challenge/fBJyQSe5Jmbm9hPAG')
+{
+    function sumPolygon(n) {
+        return (n - 2) * 180;
+    }
+    console.log(sumPolygon(3));
+    console.log(sumPolygon(4));
+    console.log(sumPolygon(6));
+
+}
+console.groupEnd()
+
+console.groupCollapsed()
+{
+
+
+
+
+
+}
+console.groupEnd()
+console.group()
+{
+
+
+
+
+
+}
+console.groupEnd()
+console.group()
+{
+
+
+
+
+
+}
+console.groupEnd()
+console.group()
+{
+
+
+
+
+
+}
+console.groupEnd()
+console.group()
+{
+
+
+
+
+
+}
+console.groupEnd()
+console.group()
+{
+
+
+
+
+
+}
+console.groupEnd()
+console.group()
+{
+
+
+
+
+
+}
+console.groupEnd()
+console.group()
+{
+
+
+
+
+
+}
+console.groupEnd()
+console.group()
+{
+
+
+
+
+
+}
+console.groupEnd()
+console.group()
+{
+
+
+
+
+
+}
+console.groupEnd()
+
+
+

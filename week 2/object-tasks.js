c32fefe4cc13999153fe0658446edbc0b03baa90
viltemeske <meshkenai@gmail.com>
2022-11-30 11:48:40 +0200

    week 2: object-tasks (7)

--- week 2/object-tasks.js
+++ week 2/object-tasks.js
@@ -17,11 +17,11 @@ console.groupEnd();
 console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
 {
     function addName(obj, name, value) {
-        return {...obj, [name]: value};
+        return { ...obj, [name]: value };
     }
-console.log(addName({}, "Brutus", 300));
-console.log(addName({ piano: 500 }, "Brutus", 400));
-console.log(addName({ piano: 500, stereo: 300 }, "Caligula", 440));    
+    console.log(addName({}, "Brutus", 300));
+    console.log(addName({ piano: 500 }, "Brutus", 400));
+    console.log(addName({ piano: 500, stereo: 300 }, "Caligula", 440));
 }
 console.groupEnd();
 
